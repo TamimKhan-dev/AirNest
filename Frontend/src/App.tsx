e9cd714e3d688257cd1ee5925bf028511e04a40e
Tasnim813 <tasnim3j@gmail.com>
2026-02-25 15:39:09 +0600

    red

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-4xl font-bold'>I am Tasnim </h1>
+      <h1 className='text-4xl font-bold text-red-400'>I am Tasnim </h1>
     </>
   )
 }
